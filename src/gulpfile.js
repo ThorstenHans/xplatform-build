acfd7052347c4d5435d004aaddc8a2d88a477ea1
Thorsten Hans <thorsten.hans@gmail.com>
2015-10-21 22:24:11 +0200

    added iife

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,35 +1,38 @@
-var gulp = require('gulp');
+(function(module){
+    
+    var gulp = require('gulp');
 
-var tasks = {
-    del: require('del'),
-    concat: require('gulp-concat'),
-    inject: require('gulp-inject'),
-    cssmin: require('gulp-cssmin'),
-    ngAnnotate: require('gulp-ng-annotate'),
-    ngTemplateCache: require('gulp-angular-templatecache'),
-    rename: require('gulp-rename'),
-    shelljs: require('shelljs'),
-    uglify: require('gulp-uglify'),
-    path: require('path'),
-    NwBuilder: require('nw-builder'),
-    inSequence: require('run-sequence')
-};
+    var tasks = {
+        del: require('del'),
+        concat: require('gulp-concat'),
+        inject: require('gulp-inject'),
+        cssmin: require('gulp-cssmin'),
+        ngAnnotate: require('gulp-ng-annotate'),
+        ngTemplateCache: require('gulp-angular-templatecache'),
+        rename: require('gulp-rename'),
+        shelljs: require('shelljs'),
+        uglify: require('gulp-uglify'),
+        path: require('path'),
+        NwBuilder: require('nw-builder'),
+        inSequence: require('run-sequence')
+    };
 
-var customGulpTasks = require('require-dir')('./gulptasks');
+    var customGulpTasks = require('require-dir')('./gulptasks');
 
-for (var gulpTask in customGulpTasks) {
-    customGulpTasks[gulpTask].init(gulp, tasks);
-}
-
-gulp.task('help', function() {
-    console.log('Execute one of the following commands\n');
     for (var gulpTask in customGulpTasks) {
-        if (!customGulpTasks[gulpTask].hasOwnProperty('docs')) {
-            continue;
-        }
-        customGulpTasks[gulpTask].docs.map(function(doc) {
-            console.log("gulp " + doc.task + " - (" + doc.description + ")");
-        });
+        customGulpTasks[gulpTask].init(gulp, tasks);
     }
-    console.log('\n');
-});
+
+    gulp.task('help', function() {
+        console.log('Execute one of the following commands\n');
+        for (var gulpTask in customGulpTasks) {
+            if (!customGulpTasks[gulpTask].hasOwnProperty('docs')) {
+                continue;
+            }
+            customGulpTasks[gulpTask].docs.map(function(doc) {
+                console.log("gulp " + doc.task + " - (" + doc.description + ")");
+            });
+        }
+        console.log('\n');
+    });
+})(module);
